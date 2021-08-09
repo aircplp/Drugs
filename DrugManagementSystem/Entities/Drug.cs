cdd1573fe15f37d10e9298af0d31562570fe2192
Adilson Mata <Adilson.Mata@gmail.com>
2021-08-09 14:30:39 +0200

    1. Drug and DrugContext 2. Test Project

---
+++ DrugManagementSystem/Entities/Drug.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DrugManagementSystem.Entities
+{
+    public class Drug
+    {
+        [StringLength(30)]
+        public string Code { get; set; }
+
+        [StringLength(100)]
+        public string Label { get; set; }
+
+        public string Description { get; set; }
+
+        [Range(0.0, (Double) Decimal.MaxValue)]
+        public decimal Price { get; set; }
+    }
+}
---
+++ DrugManagementSystem/Entities/DrugContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrugManagementSystem.Entities
+{
+    public class DrugContext: DbContext
+    {
+        public DbSet<Drug> Drugs { get; set; }
+
+    }
+}
